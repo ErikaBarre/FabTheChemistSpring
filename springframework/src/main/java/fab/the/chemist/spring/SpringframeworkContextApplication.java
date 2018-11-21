337b6375b256f2c86cc0a683384e8c02a4b78c97
fabrice <fabrice@schopenhauer>
2018-11-21 23:58:57 +0100

    11

--- springframework/src/main/java/fab/the/chemist/spring/SpringframeworkContextApplication.java
+++ springframework/src/main/java/fab/the/chemist/spring/SpringframeworkContextApplication.java
@@ -1,7 +1,7 @@
 package fab.the.chemist.spring;
 
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContext; 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
